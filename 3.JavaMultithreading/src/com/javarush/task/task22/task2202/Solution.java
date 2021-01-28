21684a3663c18bdb0b9d743ba54066bb693a1e38
damitin <ppddnk@gmail.com>
2021-01-28 21:55:14 +0300

    3 - 2202, 2203

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task22.task2202;
+
+/* 
+Найти подстроку
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
+    }
+
+    public static String getPartOfString(String string) {
+        if (string == null) throw new TooShortStringException();
+
+        String[] words = string.split(" ");
+        if (words.length < 5) throw new TooShortStringException();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i < 5; i++) {
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString().trim();
+    }
+
+    public static class TooShortStringException extends RuntimeException {
+    }
+}
