48878c76f9e3afbe8f76e67f185fd2879f27c360
damitin <ppddnk@gmail.com>
2021-03-29 08:37:32 +0300

    3 - 2909 - 3

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -9,6 +9,8 @@ public class Student extends Human {
     private String university;
     private Date beginningOfSession;
     private Date endOfSession;
+    private int course;
+
 
     public Student(String name, int age, double averageGrade) {
         super(name, age);
@@ -28,6 +30,10 @@ public class Student extends Human {
 
     public void setUniversity(String university) {
         this.university = university;
+    }
+
+    public int getCourse() {
+        return course;
     }
 
     public void printData() {
