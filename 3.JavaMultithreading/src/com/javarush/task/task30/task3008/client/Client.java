f25a5c4ec0e8355e36c5e9b8a3ad4c325fbc3607
damitin <ppddnk@gmail.com>
2021-05-17 21:30:57 +0300

    3 - 3008 - 15

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -27,6 +27,27 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            // Выводим текст сообщения в консоль
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName) {
+            // Выводим информацию о добавлении участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' присоединился к чату.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            // Выводим информацию о выходе участника
+            ConsoleHelper.writeMessage("Участник '" + userName + "' покинул чат.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
     }
 
     protected SocketThread getSocketThread() {
@@ -44,5 +65,41 @@ public class Client {
 
     protected boolean shouldSendTextFromConsole() {
         return true;
+    }
+
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        // Помечаем поток как daemon
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+            return;
+        }
+
+        if (clientConnected)
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        else
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+
+        // Пока не будет введена команда exit, считываем сообщения с консоли и отправляем их на сервер
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+            if (text.equalsIgnoreCase("exit"))
+                break;
+
+            if (shouldSendTextFromConsole())
+                sendTextMessage(text);
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
     }
 }
