72c956a2745ed47410ae657222a92152444c2ba9
damitin <ppddnk@gmail.com>
2021-06-19 11:38:08 +0300

    3 - 3110

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите полный путь файла архива:");
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(reader.readLine()));
+
+        System.out.println("Введите полное имя файла для архивации:");
+        zipFileManager.createZip(Paths.get(reader.readLine()));
+    }
+}
